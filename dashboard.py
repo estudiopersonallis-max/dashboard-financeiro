6f0c7643cac3f04411a05487a1f4c5275ace354f
estudiopersonallis-max <estudiopersonallis@gmail.com>
2026-02-06 11:20:25 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -27,7 +27,7 @@ for file in uploaded_files:
     df = pd.read_excel(file, sheet_name=sheet_name)
     df.columns = df.columns.str.strip()
 
-    df["Data"] = pd.to_datetime(df["Data"], errors="coerce")
+   df["Data"] = pd.to_datetime(df["Data"], dayfirst=True, errors="coerce")
     df = df.dropna(subset=["Data"])
 
     df["Valor_Correto"] = pd.to_numeric(df["Valor"], errors="coerce").fillna(0)
